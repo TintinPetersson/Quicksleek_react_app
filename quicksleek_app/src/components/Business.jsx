e8140e00387b58e6ab35c031038cbcf4ccebf97f
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2024-02-02 14:24:20 +0100

    feat: updated some code to test if it works better on iOS.

--- quicksleek_app/src/components/Business.jsx
+++ quicksleek_app/src/components/Business.jsx
@@ -33,24 +33,28 @@ const Business = () => (
         Fokusera på ditt företag, <br className="sm:block hidden" />
         vi sköter din närvaro online.
       </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Välj oss för skräddarsydda hemsidor som perfekt reflekterar ditt
-        varumärke, med blixtsnabb prestanda och sökmotoroptimering som
-        överträffar standardlösningarna från WordPress och Wix. <br />
+      <div className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        <p>
+          Välj oss för skräddarsydda hemsidor som perfekt reflekterar ditt
+          varumärke, med blixtsnabb prestanda och sökmotoroptimering som
+          överträffar standardlösningarna från WordPress och Wix.
+        </p>
         <br />
-        Vår användning av ledande teknologier som React och Next.js garanterar
-        unika digitala upplevelser som inte bara engagerar utan också står ut
-        från mängden, vilket erbjuder en mer skräddarsydd och professionell
-        närvaro online än traditionella webbplatsbyggare.
-      </p>
-      <RevealY duration={0.8} startPos={30} delay={0.2} once={false}>
-        <Button
-          styles="hover-animation mt-10 bg-purple-blue-gradient text-white font-normal text-base"
-          href={"#footer"}
-          name={"Ta kontakt"}
-          type={"button"}
-        />
-      </RevealY>
+        <p>
+          Vår användning av ledande teknologier som React och Next.js garanterar
+          unika digitala upplevelser som inte bara engagerar utan också står ut
+          från mängden, vilket erbjuder en mer skräddarsydd och professionell
+          närvaro online än traditionella webbplatsbyggare.
+        </p>
+        <RevealY duration={0.8} startPos={30} delay={0.2} once={false}>
+          <Button
+            styles="hover-animation mt-10 bg-purple-blue-gradient text-white font-normal text-base"
+            href={"#footer"}
+            name={"Ta kontakt"}
+            type={"button"}
+          />
+        </RevealY>
+      </div>
     </div>
 
     <div className={`${layout.sectionImg} flex-col`}>
